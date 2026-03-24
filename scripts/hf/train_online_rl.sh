7a386f4fceddc73a9c3a3909057feaf23c3b233a
linfeng-du <linfeng9918@gmail.com>
2026-03-24 07:18:45 -0400

    update script

--- scripts/hf/train_online_rl.sh
+++ scripts/hf/train_online_rl.sh
@@ -3,13 +3,15 @@
 time='01-00'
 tasks='tf8,tf10,ant,dkitty'
 
-TEMP=$(getopt --options '' --longoptions 'time:,tasks:' --name "$0" -- "$@")
+LONGOPTIONS='time:,tasks:,host:'
+TEMP=$(getopt --options '' --longoptions "${LONGOPTIONS}" --name "$0" -- "$@")
 eval set -- "${TEMP}"
 
 while true; do
   case "$1" in
     --time) time="$2"; shift 2 ;;
     --tasks) tasks="$2"; shift 2 ;;
+    --host) host="$2"; shift 2 ;;
     --) shift; break ;;
     *) echo "Unknown option: $1" >&2; exit 1 ;;
   esac
@@ -24,7 +26,8 @@ for task in "${tasks[@]}"; do
   wrap_cmds=(
     'source ~/.bashrc;'
     'activate llm4bbo;'
-    "python -m llm4bbo.trainer.hf.online_rl_trainer task=${task}"
+    "python -m llm4bbo.trainer.hf.online_rl_trainer "
+    "task=${task} grpo_config.vllm_server_host=${host}"
   )
   wrap_cmd="${wrap_cmds[*]}"
 
@@ -33,6 +36,7 @@ for task in "${tasks[@]}"; do
     --job-name="${job_name}" \
     --time="${time}" \
     --gpus-per-node='1' \
+    --exclude="${host}" \
     --output="${log_dir}/%j.out" \
     --error="${log_dir}/%j.err" \
     --wrap="${wrap_cmd}"

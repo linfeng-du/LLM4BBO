fefec9af2bbf44e7a1177475d26a61afb80d51ed
linfeng-du <linfeng9918@gmail.com>
2026-07-15 01:30:01 -0400

    Update generate

--- src/llm4bbo/generate/typings.py
+++ src/llm4bbo/generate/typings.py
@@ -2,4 +2,6 @@ from vllm import RequestOutput
 
 
 ColocateOutput = list[RequestOutput]
-ServerOutput = dict[str, list[list[int]] | list[list[float]]]
+ServerOutput = (
+    dict[str, list[list[int]] | list[list[list[int]]] | list[list[list[float]]]]
+)

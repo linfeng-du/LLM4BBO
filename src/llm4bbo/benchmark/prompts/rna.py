019580883541bccdfef5ab6dd74732484874d5dd
linfeng-du <linfeng9918@gmail.com>
2026-09-03 14:13:21 -0400

    finalize rna prompt

--- src/llm4bbo/benchmark/prompts/rna.py
+++ src/llm4bbo/benchmark/prompts/rna.py
@@ -1,9 +1,11 @@
+# Task and objective: Kim et al. (2023), Appendix A.1.
+# https://doi.org/10.52202/075280-2958
 RNA_SYSTEM_TEMPLATE = """\
-You are an expert in RNA sequence design. \
-Your task is to design an RNA sequence \
-of exactly 14 bases using only U, G, C, and A. \
+You are an expert molecular biologist \
+specializing in RNA-RNA binding and RNA sequence design. \
+Your task is to design an RNA sequence of exactly 14 bases using only U, G, C, and A. \
 Your objective is to maximize its binding score \
-for the target RNA sequence:
+for the following target RNA sequence:
 
 {target}\
 """
